a30fa85eb6fe28795b6b3e21007a12c1943a1fdd
ChanNyein AungAung <44551834+aungaungchannyein@users.noreply.github.com>
2024-10-31 16:42:31 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>&copy; Copyright by Chan Nyein Aung Aung. All rights reserved.</small>
       <small>Last checked limits: 17 days ago</small>
     </footer>
   );
